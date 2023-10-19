232a66437a2f41d1b2831d68f6ba09fa933f3c31
SHASHIBHUSHAN KUMAR <91677440+Shashi31032004@users.noreply.github.com>
2023-10-19 11:32:00 +0530

    java
    java

---
+++ level_of_nodes.java
@@ -0,0 +1,67 @@
+
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V + 1; i++)
+                list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+                list.get(v).add(u);
+            }
+            int X = sc.nextInt();
+
+            Solution ob = new Solution();
+
+            System.out.println(ob.nodeLevel(V, list, X));
+        }
+    }
+}
+// } Driver Code Ends
+
+/* Complete the function below */
+
+class Solution {
+    // Function to find the level of node X.
+    int nodeLevel(int V, ArrayList<ArrayList<Integer>> adj, int X) {
+        int level = 0;
+
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[V];
+
+        q.add(0);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                int nn = q.poll();
+                visited[nn] = true;
+
+                if (nn == X)
+                    return level;
+
+                for (var neighbor : adj.get(nn)) {
+                    if (visited[neighbor] == false)
+                        q.add(neighbor);
+                }
+            }
+
+            level++;
+        }
+
+        return -1;
+    }
+}
